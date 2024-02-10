373e212bb171234ce08bad30d695e51a2085796f
Antonio-Lima <gutto1998@gmail.com>
2024-02-09 22:22:43 -0300

    Routes created.

--- src/routes/public.tsx
+++ src/routes/public.tsx
@@ -1,12 +1,15 @@
 import { createStackNavigator } from "@react-navigation/stack";
 import { PUBLIC } from "../constants/screens";
-import Login from "../pages/public/login";
+import Login from "../pages/public/Login/login";
 
 const Stack = createStackNavigator();
 
 export default function PublicRoutes() {
   return (
-    <Stack.Navigator screenOptions={{ headerShown: false }}>
+    <Stack.Navigator
+      screenOptions={{ headerShown: false }}
+      initialRouteName={PUBLIC.LOGIN}
+    >
       <Stack.Screen name={PUBLIC.LOGIN} component={Login} />
     </Stack.Navigator>
   );
