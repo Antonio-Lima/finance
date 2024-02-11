c14af7666f12a880f2b7cbc13ea07f5c8b5cde23
Antonio-Lima <gutto1998@gmail.com>
2024-02-10 22:37:05 -0300

    Welcome page created.

--- src/pages/public/Welcome/index.tsx
+++ src/pages/public/Welcome/index.tsx
@@ -1,20 +1,46 @@
+import { Text, View } from "react-native";
+import { MotiView } from "moti";
+
 import Wrapper from "../../../components/Wrapper";
+import Button from "../../../components/Button";
+
 import { colors } from "../../../constants/styles";
 
 import App from "../../../assets/images/app.svg";
-import { Text } from "react-native";
+
 import { styles } from "./styles";
+import { useNavigation } from "@react-navigation/native";
 
 export default function Welcome() {
+  const navigation = useNavigation();
   return (
     <Wrapper backgroundColor={colors.mainBrand}>
-      <App
-        height="200"
-        style={{
-          marginTop: 40,
+      <MotiView
+        style={styles.main}
+        from={{ opacity: 0, translateY: 100 }}
+        animate={{ opacity: 1, translateY: 0 }}
+        transition={{
+          type: "timing",
+          duration: 1000,
         }}
-      />
-      <Text style={styles.title}>Welcome</Text>
+      >
+        <App height={250} />
+        <Text style={styles.title}>Welcome</Text>
+        <Text style={styles.subtitle}>to your finance app</Text>
+        <View style={styles.container}>
+          <Button
+            label="Login"
+            variant="primary"
+            onPress={() => navigation.navigate("login")}
+          />
+          <Button
+            label="Register"
+            variant="outline-blue"
+            onPress={() => console.log("Register")}
+          />
+        </View>
+        <MotiView style={styles.background} />
+      </MotiView>
     </Wrapper>
   );
 }
--- src/pages/public/Welcome/styles.ts
+++ src/pages/public/Welcome/styles.ts
@@ -2,10 +2,34 @@ import { StyleSheet } from "react-native";
 import { colors } from "../../../constants/styles";
 
 export const styles = StyleSheet.create({
+  main: {
+    width: "100%",
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
   title: {
-    marginVertical: 40,
-    color: colors.white,
+    marginTop: 40,
+    color: colors.mainBrand,
     fontSize: 32,
     fontWeight: "bold",
+  },
+  subtitle: {
+    color: colors.mainBrand,
+    fontSize: 20,
+  },
+  container: {
+    marginTop: 80,
+    width: "90%",
+    gap: 20,
+  },
+  background: {
+    position: "absolute",
+    width: "200%",
+    height: "110%",
+    top: "40%",
+    backgroundColor: colors.white,
+    transform: [{ rotate: "60deg" }],
+    zIndex: -1,
   },
 });
