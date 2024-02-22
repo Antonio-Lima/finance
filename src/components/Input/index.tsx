097100b5e8aa2dcf8c0938bb6f58f977044c5290
Antonio-Lima <gutto1998@gmail.com>
2024-02-22 14:56:50 -0300

    Development of Input Component started.

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,9 +1,31 @@
-import { TextInput, TextInputProps } from "react-native";
+import { TextInput, TextInputProps, View } from "react-native";
 import { styles } from "./styles";
+import { MotiText } from "moti";
+import { useState } from "react";
 
 interface InputProps extends TextInputProps {}
 
 export default function Input(props: InputProps) {
-  const { placeholder } = props;
-  return <TextInput placeholder={placeholder} style={styles.input} />;
+  const { value, placeholder, ...rest } = props;
+  const [focus, setFocus] = useState(Boolean(value));
+  return (
+    <View style={styles.container}>
+      <MotiText
+        style={styles.label}
+        animate={{
+          translateY: focus ? -20 : 0,
+        }}
+        transition={{ type: "timing" }}
+      >
+        {placeholder}
+      </MotiText>
+      <TextInput
+        style={styles.input}
+        onFocus={() => setFocus(true)}
+        onBlur={() => setFocus(Boolean(value))}
+        value={value}
+        {...rest}
+      />
+    </View>
+  );
 }
--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,12 +1,25 @@
 import { StyleSheet } from "react-native";
-import { colors } from "../../constants/styles";
+import { colors, dimensions, font } from "../../constants/styles";
 
 export const styles = StyleSheet.create({
   input: {
     width: "100%",
-    borderBottomWidth: 1,
-    borderBottomColor: colors.white,
-    paddingBottom: 5,
-    fontSize: 16,
+    fontSize: font.size.md,
+  },
+  container: {
+    width: "100%",
+    position: "relative",
+    borderWidth: 2,
+    borderColor: colors.mainBrand,
+    borderRadius: dimensions.radius.s,
+    padding: 8,
+  },
+  label: {
+    position: "absolute",
+    fontSize: font.size.md,
+    color: colors.gray["500"],
+    backgroundColor: colors.white,
+    paddingHorizontal: 4,
+    margin: 8,
   },
 });
