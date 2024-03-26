c3341bbfade2a3bf57fd7520bb4cfa62d400a95a
Antonio-Lima <gutto1998@gmail.com>
2024-03-26 14:56:22 -0300

    Splash screen implemented in the navigation controller.

--- src/routes/public.tsx
+++ src/routes/public.tsx
@@ -2,6 +2,7 @@ import { createStackNavigator } from "@react-navigation/stack";
 import { PUBLIC } from "../constants/screens";
 import Login from "../pages/public/Login";
 import Welcome from "../pages/public/Welcome";
+import Splash from "../pages/public/Splash";
 
 const Stack = createStackNavigator();
 
@@ -9,8 +10,9 @@ export default function PublicRoutes() {
   return (
     <Stack.Navigator
       screenOptions={{ headerShown: false }}
-      initialRouteName={PUBLIC.WELCOME}
+      initialRouteName={PUBLIC.SPLASH}
     >
+      <Stack.Screen name={PUBLIC.SPLASH} component={Splash} />
       <Stack.Screen name={PUBLIC.WELCOME} component={Welcome} />
       <Stack.Screen name={PUBLIC.LOGIN} component={Login} />
     </Stack.Navigator>
